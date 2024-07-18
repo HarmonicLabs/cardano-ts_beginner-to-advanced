bbf5cfc2d6cbe946e3e6dc912fe504443253a248
Michele Nuzzi <michele.nuzzi.2014@gmail.com>
2024-07-18 18:25:05 +0200

    ep2

---
+++ Part 1: practical introduction to the UTxO model/what_is_inside_an_utxo/src/index.ts
@@ -0,0 +1,14 @@
+import { Address, DataConstr, DataI, TxOut, UTxO, Value } from "@harmoniclabs/plu-ts";
+
+void async function main()
+{
+    const addr = Address.fromString("addr_test1qzq55vqf303tduqa0f6r4rmamt2lxw5c98yp5rcyekl6aupgrkxchwfa7uzxtc4sssn4hdp8pdhpe0gvnl3tec8yzjsq5enqa4");
+
+    const txOut = new TxOut({
+        address: addr,
+        value: Value.lovelaces( 0 ),
+        datum: new DataI( 0 ),
+        refScript: undefined
+    });
+
+}();
